b78ccfc8517135ebd5f17e058be1290b84d12cc8
Roelof Roos <github@roelof.io>
2020-05-11 15:42:51 +0200

    Opgave 3

---
+++ Makefile
@@ -0,0 +1,8 @@
+"ICTSE2g RoelofRoos S1073508.zip": gulpfile.js/ spec/ src/ vendor/ package.json
+	zip -r0 \
+	    "ICTSE2g RoelofRoos S1073508.zip" \
+	    gulpfile.js/ \
+	    spec/ \
+	    src/ \
+	    vendor/ \
+	    package.json
--- vendor/memes/memes.js
+++ vendor/memes/memes.js
@@ -2,5 +2,42 @@
 * Een constructor function is ook toegestaan.
 **/
 class Meme {
-    //opdracht voor student
+
+    constructor(memeList) {
+        if (!memeList || !Array.isArray(memeList) || memeList.length === 0) {
+            throw Error('Invalid meme list provided')
+        }
+
+        this.memeList = memeList
+        this.cursor = 0
+    }
+
+    getNextMeme () {
+        const meme = this.memeList[this.cursor]
+        this.cursor++
+        if (this.cursor >= this.memeList.length) {
+            this.cursor = 0
+        }
+        return meme
+    }
+
+    searchMeme (search) {
+        const results = []
+        search = `${search}`.trim().toLowerCase()
+        this.memeList.forEach((meme) => {
+            // Skip invalids
+            if (!meme.name) {
+                return
+            }
+
+            // Find meme
+            const memeName = String(meme.name).toLowerCase()
+            if (memeName.indexOf(search) !== -1) {
+                results.push(meme)
+            }
+        })
+
+        return results
+
+    }
 }
